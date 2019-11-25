537022cf1ceed46e59da0bc8bcfe0de891c3083c
Max Lloyd <maxjtlloyd@gmail.com>
2019-11-25 20:36:11 +0000

    Menu proof of concept, no functionality

--- core/src/com/kroy/game/MainMenuScreen.java
+++ core/src/com/kroy/game/MainMenuScreen.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainMenuScreen implements Screen {
+
+    private static final int BUTTON_WIDTH = 300;
+    private static final int BUTTON_HEIGHT = 150;
+
+
 
     KROY game = new KROY();
 
@@ -14,6 +20,7 @@ public class MainMenuScreen implements Screen {
     Texture playButtonInactive;
     Texture exitButtonActive;
     Texture exitButtonInactive;
+    OrthographicCamera camera;
 
     public MainMenuScreen(KROY game){
         this.game = game;
@@ -34,11 +41,13 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         game.batch.begin();
         //game.batch.draw(img, x, y);
-        game.batch.end();
 
-        game.batch.draw(exitButtonActive, 100, 100);
+        game.batch.draw(playButtonActive, (GameScreen.WIDTH/2) - BUTTON_WIDTH/2, 400, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.batch.draw(exitButtonActive, (GameScreen.WIDTH/2) - BUTTON_WIDTH/2, 100, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.batch.end();
 
     }
 
--- desktop/src/com/kroy/game/desktop/DesktopLauncher.java
+++ desktop/src/com/kroy/game/desktop/DesktopLauncher.java
@@ -4,10 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kroy.game.GameScreen;
 import com.kroy.game.MainMenuScreen;
+import com.kroy.game.KROY;
 
 public class DesktopLauncher {
-	public void main(String[] arg) {
+	public static void main(String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainMenuScreen(), config);
+		config.width = GameScreen.WIDTH;
+		config.height = GameScreen.HEIGHT;
+		config.resizable = false;
+		new LwjglApplication(new KROY(), config);
 	}
 }
