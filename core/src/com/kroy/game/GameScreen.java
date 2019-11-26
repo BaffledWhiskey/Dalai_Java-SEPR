8e29ef63aaafbc01471e09cfd94ac1566e61b2e3
Max Lloyd <maxjtlloyd@gmail.com>
2019-11-26 11:50:23 +0000

    Added GameScreen

---
+++ core/src/com/kroy/game/GameScreen.java
@@ -0,0 +1,63 @@
+package com.kroy.game;
+
+
+import java.util.Iterator;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Screen;
+
+
+public class GameScreen implements Screen {
+
+	SpriteBatch batch;
+	Texture img;
+	KROY game;
+	public static int WIDTH = 480;
+	public static int HEIGHT = 720;
+
+	public GameScreen(KROY game) {
+		this.game = game;
+
+	}
+
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
