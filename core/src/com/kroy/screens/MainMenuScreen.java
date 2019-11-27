12a5dfb3a6d52972fbf27f25f01050dbeceb10f5
Max Lloyd <maxjtlloyd@gmail.com>
2019-11-27 14:36:30 +0000

    Main menu functioning (placeholder logo and buttons)

--- core/src/com/kroy/screens/MainMenuScreen.java
+++ core/src/com/kroy/screens/MainMenuScreen.java
@@ -11,6 +11,12 @@ public class MainMenuScreen implements Screen {
 
     private static final int BUTTON_WIDTH = 300;
     private static final int BUTTON_HEIGHT = 150;
+    private static final int x = GameScreen.WIDTH/2;
+    private static final int EXIT_BUTTON_Y = 100;
+    private static final int PLAY_BUTTON_Y = 300;
+    private static final int KROY_LOGO_Y = 500;
+    private static final int LOGO_WIDTH = 400;
+    private static final int LOGO_HEIGHT = 200;
 
 
 
@@ -20,6 +26,7 @@ public class MainMenuScreen implements Screen {
     Texture playButtonInactive;
     Texture exitButtonActive;
     Texture exitButtonInactive;
+    Texture kroyLogo;
     OrthographicCamera camera;
 
     public MainMenuScreen(KROY game){
@@ -28,6 +35,7 @@ public class MainMenuScreen implements Screen {
         playButtonInactive = new Texture("playButton.png");
         exitButtonActive = new Texture("exitButtonActive.png");
         exitButtonInactive = new Texture("exitButton.png");
+        kroyLogo = new Texture("KROY_logo.png");
     }
 
 
@@ -39,14 +47,30 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.25f, 0.25f, 0.25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-        //game.batch.draw(img, x, y);
+        game.batch.draw(kroyLogo, x - LOGO_WIDTH/2, KROY_LOGO_Y, LOGO_WIDTH, LOGO_HEIGHT);
 
-        game.batch.draw(playButtonActive, (GameScreen.WIDTH/2) - BUTTON_WIDTH/2, 400, BUTTON_WIDTH, BUTTON_HEIGHT);
-        game.batch.draw(exitButtonActive, (GameScreen.WIDTH/2) - BUTTON_WIDTH/2, 100, BUTTON_WIDTH, BUTTON_HEIGHT);
+        if(Gdx.input.getX() < x + BUTTON_WIDTH && Gdx.input.getX() > x - BUTTON_WIDTH && GameScreen.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + BUTTON_HEIGHT && GameScreen.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y){
+            game.batch.draw(playButtonActive, (x) - BUTTON_WIDTH/2, PLAY_BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if(Gdx.input.isTouched()){
+                this.dispose();
+                game.setScreen(new GameScreen(game));
+            }
+        }else {
+            game.batch.draw(playButtonInactive, (x) - BUTTON_WIDTH / 2, PLAY_BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
+
+        if(Gdx.input.getX() < x + BUTTON_WIDTH && Gdx.input.getX() > x - BUTTON_WIDTH && GameScreen.HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + BUTTON_HEIGHT && GameScreen.HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y){
+            game.batch.draw(exitButtonActive, (x) - BUTTON_WIDTH/2, EXIT_BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if(Gdx.input.isTouched()){
+                Gdx.app.exit();
+            }
+        }else {
+            game.batch.draw(exitButtonInactive, (x) - BUTTON_WIDTH / 2, EXIT_BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
         game.batch.end();
 
     }
