3f182e5350e9b64602be60f4848b0245bcb30bbe
jkershaw2000 <jackoliver2002>
2019-11-28 15:15:36 +0000

    Added Filed Required for the map to Render

--- core/src/com/kroy/screens/GameScreen.java
+++ core/src/com/kroy/screens/GameScreen.java
@@ -1,6 +1,5 @@
 package com.kroy.screens;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,52 +10,57 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class GameScreen implements Screen {
 
-	private OrthographicCamera camera;
-	private OrthogonalTiledMapRenderer renderer;
-	private TiledMap map;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
 
-	public static int WIDTH = 720;
-	public static int HEIGHT = 480;
-	@Override
-	public void render(float delta){
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		renderer.render();
-	}
-
-	@Override
-	public void show(){
-		map = new TmxMapLoader().load("maps/Map.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map);
-	}
-
-	@Override
-	public void resize(int width, int height) {
-
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
+    public static int WIDTH = 1280;
+    public static int HEIGHT = 720;
 
 
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0,0,0,1) ;
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.setMap(map);
+        renderer.setView(camera);
+        renderer.render();
+    }
+    @Override
+    public void resize(int width, int height){
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
 
-	public void hide(){
-		dispose();
-	}
+    @Override
+    public void show(){
+        map = new TmxMapLoader().load("maps/Map.tmx");
 
-	@Override
-	public void dispose() {
-		map.dispose();
-		renderer.dispose();
-	}
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        camera = new OrthographicCamera();
+    }
 
 
+    @Override
+    public void hide(){
+        dispose();
+    }
+
+    @Override
+    public void pause(){
+
+    }
+
+    @Override
+    public void resume(){
+
+    }
+
+    @Override
+    public void dispose(){
+        map.dispose();
+        renderer.dispose();
+    }
 }
