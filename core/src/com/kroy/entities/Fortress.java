e7106a01de4dc3160aac6704dcb149e410afd06a
yuqinggong97 <44002374+yuqinggong97@users.noreply.github.com>
2019-11-29 10:14:08 +0000

    added return

--- core/src/com/kroy/entities/Fortress.java
+++ core/src/com/kroy/entities/Fortress.java
@@ -1,5 +1,27 @@
 package com.kroy.entities;
 
-public class Fortress {
+import com.kroy.game.Point;
+
+public class Fortress extends Tower{
+    /**
+     * @param dimensions The size of the tower
+     * @param health     The current health of the tower
+     * @param range      The maximun range of the tower
+     * @param position   The position of the tower on the map
+     */
+
+    public Fortress(int[] dimensions, int health, int range, Point position) {
+        super(dimensions, health, range, position);
+    }
+
+    public int increaseHealth(){
+        return health++;
+    }
+
+
+
+    public void attackFireEngine(FireEngine engine, int healthChangePerSecond){
+
+    }
 
 }
