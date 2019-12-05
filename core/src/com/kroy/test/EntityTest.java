abd54dac8c0f40bae27f7731e7ff9dc45df21442
Peter <peter_clark@talktalk.net>
2019-12-05 14:28:16 +0000

    Added commented out JUnit test (it doesn't work and idk why)

---
+++ core/src/com/kroy/test/EntityTest.java
@@ -0,0 +1,20 @@
+package com.kroy.test;
+
+ /**import com.kroy.entities.Entity;
+ import com.kroy.game.Point;
+ import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.Assertions;
+
+ class EntityTest {
+
+@Test
+public void testInRangeBoundary() {
+    Entity e = new Entity(0,5, new Point(5,5));
+    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(9,0))));
+    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(10,0))));
+    Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(11,0))));
+    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,9))));
+    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,10))));
+    Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(0,11))));
+}
+}**/
