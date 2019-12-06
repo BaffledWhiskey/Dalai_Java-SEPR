38a0459bc33da099155411f046d82f2f1fd494c2
Peter <peter_clark@talktalk.net>
2019-12-06 14:28:05 +0000

    Spent an hour trying to get JUnit to work and basically getting nowhere, changed a few things though

--- core/src/com/kroy/test/EntityTest.java
+++ core/src/com/kroy/test/EntityTest.java
@@ -5,16 +5,16 @@ package com.kroy.test;
  import org.junit.jupiter.api.Test;
  import org.junit.jupiter.api.Assertions;
 
- class EntityTest {
+class EntityTest {
 
 @Test
-public void testInRangeBoundary() {
-    Entity e = new Entity(0,5, new Point(5,5));
-    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(9,0))));
-    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(10,0))));
-    Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(11,0))));
-    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,9))));
-    Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,10))));
-    Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(0,11))));
-}
+    public void testInRangeBoundary() {
+        Entity e = new Entity(0,5, new Point(5,5));
+        Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(9,0))));
+        Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(10,0))));
+        Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(11,0))));
+        Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,9))));
+        Assertions.assertTrue(e.inRange(new Entity(0,0,new Point(0,10))));
+        Assertions.assertFalse(e.inRange(new Entity(0,0,new Point(0,11))));
+    }
 }**/
