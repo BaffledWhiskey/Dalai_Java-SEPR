5ab0a50496ac3616972dd564af0e0bd1c4eca46d
PeteLeeds <56679863+PeteLeeds@users.noreply.github.com>
2020-01-15 20:49:21 +0000

    Edited FireEngineTest

--- core/test/FireEngineTest.java
+++ core/test/FireEngineTest.java
@@ -1,4 +1,7 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kroy.entities.FireEngine;
 import com.kroy.game.Point;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -11,32 +14,49 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.stubbing.BaseStubbing;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SpriteBatch.class})
 public class FireEngineTest {
 
     @Mock
     public Texture mockedImg;
     public Animation mockedAnimation;
+    public FireEngine f;
+    public SpriteBatch mockBatch;
 
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         mockedImg = mock(Texture.class);
         mockedAnimation = mock(Animation.class);
         when(mockedImg.getWidth()).thenReturn(0);
         when(mockedImg.getHeight()).thenReturn(0);
+        mockBatch = Mockito.mock(SpriteBatch.class);
+        whenNew(SpriteBatch.class).withAnyArguments().thenReturn(mockBatch);
     }
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
 
-    @Test(expected = Exception.class)
-    public void engineDoesNotDestroyWhenHealthyTest() {
+    @Test
+    public void engineDoesNotDestroyWhenHealthyTest() throws Exception {
         FireEngine f = new FireEngine(1,1,100,1,new Point(1,1),mockedImg);
+        f.destroy(mockedAnimation,1f);
+    }
+
+    @Test
+    public void engineDestroysWhenHealthBelowZero() throws Exception {
+        FireEngine f = new FireEngine(1,1,0,1,new Point(1,1),mockedImg);
         f.destroy(mockedAnimation,1f);
     }
 }
