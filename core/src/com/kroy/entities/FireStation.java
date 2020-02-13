80474a4a61368f2d803bf25fedf9b58bd1673f04
Nicolas Forstner <nicolasforstner@yahoo.de>
2020-02-13 14:58:22 +0000

    changed minigame start mechanism

--- core/src/com/kroy/entities/FireStation.java
+++ core/src/com/kroy/entities/FireStation.java
@@ -11,27 +11,62 @@ public class FireStation extends Unit {
     float repairRate;
     float refillRate;
     JsonValue miniGameJson;
-    float miniGameProbability;
+    float miniGameInterval;
+    float miniGameCountdown;
     float miniGameDifficulty = 1;
 
-    public FireStation(Kroy gameScreen, Vector2 position, float size, Sprite sprite, float health) {
-        super(gameScreen, position, size, sprite, health);
+    /**
+     * The constructor for a FireStation that can be used for testing. Note that this is not the constructor that is used in
+     * the actual game.
+     *
+     * @param kroy The Kroy instance in which the Alien lives
+     * @param position The Alien's position within the game world
+     * @param size The Alien's size. It is used for rendering
+     * @param sprite The Alien's Sprite
+     * @param health The Alien's (maximum) health. It always starts out with full health
+     * @param repairRange The range in which fire engines will be repaired and refilled
+     * @param repairRate The health per second that nearby fire engines will receive
+     * @param refillRate The water per second that nearby fire engines will receive
+     * @param miniGameJson The Json for the mini game when the fire station is contested
+     * @param miniGameInterval The probability that a mini game starts
+     * @param miniGameIntervalOffset The offset by which the mini game interval is started
+     */
+    public FireStation(Kroy kroy, Vector2 position, float size, Sprite sprite, float health, float repairRange, float repairRate, float refillRate, JsonValue miniGameJson, float miniGameInterval, float miniGameIntervalOffset) {
+        super(kroy, position, size, sprite, health);
+        this.repairRange = repairRange;
+        this.repairRate = repairRate;
+        this.refillRate = refillRate;
+        this.miniGameJson = miniGameJson;
+        this.miniGameInterval = miniGameInterval;
+        this.miniGameCountdown = miniGameIntervalOffset;
     }
 
     /**
-     * Builds a ForeStation from a JsonValue object. */
-    public FireStation(Kroy gameScreen, JsonValue json) {
-        super(gameScreen, json);
+     * Builds a FireStation from a JsonValue object. This is the constructor that is used in the game.
+     *
+     * @param kroy The Kroy instance in which the fire station lives
+     * @param json The JsonObject instance that holds the information according to which the fire station is initialized. */
+    public FireStation(Kroy kroy, JsonValue json) {
+        super(kroy, json);
         repairRange = json.getFloat("repairRange");
         repairRate = json.getFloat("repairRate");
         refillRate = json.getFloat("refillRate");
 
         miniGameJson = json.get("miniGame");
-        miniGameProbability = miniGameJson.getFloat("probability");
+        miniGameInterval = miniGameJson.getFloat("interval");
+        miniGameCountdown = miniGameJson.getFloat("intervalOffset");
     }
 
+    /**
+     * Updates the fire engine.
+     *
+     * @param deltaTime The amount of time that has passed since the last tick */
     public void update(float deltaTime) {
-        if ((Math.random() < miniGameProbability * deltaTime)) {
+
+        // Start the minigame with a slight probability
+        miniGameCountdown -= deltaTime;
+        if (miniGameCountdown < 0) {
+            miniGameCountdown = miniGameInterval;
             // With each time that we play the minigame, the difficulty increases
             miniGameDifficulty += 0.25;
             kroy.startMiniGame(this);
